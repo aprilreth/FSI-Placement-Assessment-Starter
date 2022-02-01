984e9226eb34a4a295e2cc1e42a8b1d7b615efba
April Reth <april.reth@gmail.com>
2022-02-01 18:34:44 -0500

    Adding function for Gingerbread and testing

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,32 @@
-// HINT: You can delete this console.log after you no longer need it!
-console.log('JavaScript code has loaded!')
-// First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
-
-// We'll use these variables to track the counts of each cookie type
+let yourName = "April Reth"
 let gb = 0 // Gingerbread
 let cc = 0 // Chocolate Chip
 let sugar = 0 // Sugar Sprinkle
 
 // selecting the element with an id of credit
 const credit = document.querySelector('#credit')
-// selecting the element with an id of add-gb
-const gbPlusBtn = document.querySelector('#add-gb')
 
 // Code to update name display
 credit.textContent = `Created by ${yourName}`
 
-// Event listener for clicks on the "+" button for Gingerbread cookies
-gbPlusBtn.addEventListener('click', function() {
-// HINT: You can delete this console.log after you no longer need it!
-console.log('Gingerbread + button was clicked!')
+// Gingerbread Quantity
+const gbPlusBtn = document.querySelector('#add-gb')
+function changeQuantityGb(displayQuantity) {
+    let gbQty = document.getElementById('qty-gb')
+}
 
-// TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+gbPlusBtn.addEventListener('click', function() {
+    gb = gb + 1
+    changeQuantityGb(gb)
+    qtyTotal.textContent = gb + cc + sugar
 })
 
-// TODO: Hook up event listeners for the rest of the buttons
+const gbMinusBtn = document.querySelector('#minus-gb')
+gbMinusBtn.addEventListener('click', function(){
+    if(gb > 0) {
+        gb = gb - 1
+        changeQuantityGb(gb)
+        qtyTotal.textContent = gb + cc + sugar
+    }
+})
+
